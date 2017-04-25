b60e9a2f7c24de8fd1b229c06ad7ce7c40dec5ba
Flubbex <michael_nijhof@live.com>
2017-04-25 22:06:23 +0200

    First category working

--- source/index.js
+++ source/index.js
@@ -1,1 +1,21 @@
-module.exports = {};
+var $ = require('jquery');
+
+window.loadPDF = function(uri)
+{
+    $("#reader")[0].src = uri;
+}
+
+function loadIndex(index)
+{
+    $("#reader").html(uri);
+}
+
+$(document).ready(function(){
+  $('.linktab').click(function(){
+    var linklist = $(this).next();
+    if (linklist.is(':visible'))
+      linklist.slideUp('fast');
+    else
+      linklist.slideDown('fast');
+  });
+});
