b0febee2fdcdd7708fb50e536a1a69a5470255cb
Flubbex <michael_nijhof@live.com>
2017-04-26 13:26:29 +0200

    Added Kids Section

--- source/index.js
+++ source/index.js
@@ -10,12 +10,32 @@ function loadIndex(index)
     $("#reader").html(uri);
 }
 
+function hideAllExcept(tag,callback)
+{
+    $(".linktab").slideUp('fast',function(){
+      tag.slideDown('fast',callback);
+    });
+}
+
+function showAll()
+{
+    $(".linktab").slideDown('fast');
+}
+
 $(document).ready(function(){
   $('.linktab').click(function(){
     var linklist = $(this).next();
     if (linklist.is(':visible'))
-      linklist.slideUp('fast');
+    {
+      linklist.slideUp('fast',function(){
+        showAll();
+      });
+    }
     else
-      linklist.slideDown('fast');
+    {
+      hideAllExcept($(this),function(){
+          linklist.slideDown('fast');
+        });
+    }
   });
 });
